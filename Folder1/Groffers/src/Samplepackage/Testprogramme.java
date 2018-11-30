00a6c2c6846b59589048b86e3c3a502fa9bf4e8d
Nirupama <Nirupama@192.168.2.6>
2018-11-30 21:40:12 +0530

    FirstPush

---
+++ Folder1/Groffers/src/Samplepackage/Testprogramme.java
@@ -0,0 +1,18 @@
+package Samplepackage;
+
+import org.testng.annotations.Test;
+
+public class Testprogramme 
+{
+	@Test
+	public static void testmethod()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Nirupama");
+		System.out.println("Application runs successfully");
+		System.out.println("Hi hello");
+		System.out.println("QspidersTestyantra");
+
+	}
+
+}
